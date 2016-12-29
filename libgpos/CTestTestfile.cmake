c7db67cf21a82a6a3d129de2ef19cd155e4e66b3
Bhuvnesh Chaudhary <bchaudhary@pivotal.io>
2016-12-28 17:54:21 -0800

    Merged GPOS into GPORCA
    Did the following
    - Removed all Makefile
    - Updated CMakeLists.txt for all the targets
    
    Signed-off-by: Dhanashree Kashid <dkashid@pivotal.io>

---
+++ libgpos/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /tmp/gporca
+# Build directory: /tmp/gporca/libgpos
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(libgpos)
+subdirs(libnaucrates)
+subdirs(libgpdbcost)
+subdirs(libgpopt)
+subdirs(server)
