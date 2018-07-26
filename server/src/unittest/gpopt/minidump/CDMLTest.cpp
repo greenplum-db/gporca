071b82451e2edee9e08b98f868bf3407b5fa7cde
Venkatesh Raghavan <vraghavan@pivotal.io>
2018-07-26 16:51:22 -0700

    Rename test file

--- server/src/unittest/gpopt/minidump/CDMLTest.cpp
+++ server/src/unittest/gpopt/minidump/CDMLTest.cpp
@@ -28,7 +28,7 @@ ULONG CDMLTest::m_ulDMLTestCounter = 0;  // start from first test
 // minidump files
 const CHAR *rgszDMLFileNames[] =
 	{
-	"../data/dxl/minidump/DPEIDFWithCast.mdp",
+	"../data/dxl/minidump/PartitionEliminationIDFWithCast.mdp",
 	"../data/dxl/minidump/Insert.mdp",
 	"../data/dxl/minidump/InsertRandomDistr.mdp",
 	"../data/dxl/minidump/InsertMismatchedDistrubution.mdp",
